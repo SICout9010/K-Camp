19b1e8833b0804e83f83bedb79e555a3c249c77f
noraainuse <nxx4235@gmail.com>
2025-11-01 20:12:02 +0700

    feat & fix: file url - func

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,7 @@
+import { PUBLIC_POCKETBASE_URL } from "$env/static/public";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import PocketBase, { type RecordModel } from "pocketbase";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
@@ -8,6 +10,12 @@ export function cn(...inputs: ClassValue[]) {
 // serializeNonPOJOs for Pocketbase
 export const serializeNonPOJOs = (obj: any) => {
 	return structuredClone(obj);
+}
+
+export const getFileUrl = (fileRecord: RecordModel, fileName: string) => {
+	const pb = new PocketBase(PUBLIC_POCKETBASE_URL);
+	const fileUrl = pb.files.getURL(fileRecord, fileName);
+	return fileUrl;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
